a3ff442749aa8ac0f5f5c16b6a30470286e6a2bd
SKDC_SMART <somchaibutphon@gmail.com>
2026-07-28 04:45:11 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = Object.freeze({
   APP_NAME: 'ระบบสำรวจร้านค้าคู่แข่ง',
-  API_BASE_URL: 'https://REPLACE_WITH_WORKER_URL.workers.dev',
+  API_BASE_URL: 'https://bill-api.somchaibutphon.workers.dev',
   APP_VERSION: '2026.07.26-round01-foundation-qr-login-v1'
 });
