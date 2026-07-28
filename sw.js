dfc5a26bc53618b8cf641a7dafd15aaf8b6075b7
SKDC_SMART <somchaibutphon@gmail.com>
2026-07-29 05:16:43 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='retailinsight-r05-s02';
+const CACHE='retailinsight-r05-s04';
 const ASSETS=['./','login.html','field.html','admin.html','assets/css/app.css','assets/css/admin-console.css','assets/js/config.js','assets/js/api.js','assets/js/login.js',
+  './admin-user-month-import.html','./assets/js/admin-user-month-import.js',
   './deployment-check.html',
   './assets/js/deployment-check.js'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
