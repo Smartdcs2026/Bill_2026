7516bc09226ef68778cccba34daba4020122dacf
SKDC_SMART <somchaibutphon@gmail.com>
2026-07-29 19:25:03 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='retailinsight-field-r05-s05-v2';
+const CACHE='retailinsight-r05-s05-rev03';
 const ASSETS=['./','login.html','field.html','assets/css/field-workspace.css','assets/js/config.js','assets/js/api.js','assets/js/ui-alerts.js','assets/js/date-th.js','assets/js/local-draft-db.js','assets/js/field-workspace.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
